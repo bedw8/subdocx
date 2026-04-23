7073f9334b6c998ff660ed50ef049c525ee61ba2
Benjamin Edwards <bedw@bedw.me>
2026-04-23 16:07:39 -0400

    substitution class

--- src/subdocx/substitution.py
+++ src/subdocx/substitution.py
@@ -1,90 +1,148 @@
-from typing import Callable
-from .utils import Document, iter_runs, Run
-from .template import Template  # , NHandler
-from .config import SubConfig, formatType
-import pandas as pd
+from typing import TYPE_CHECKING, Callable
 import re
+
+import pandas as pd
+
+from .config import SubConfig, formatType
 from .format import functions
+from .template import Template
+from .utils import Document, Run, iter_runs
 
-#### IDEA
-# class Substitute
-# 
-# single file 
-# __init__ -> Substitute()
-# 
-# bulk 
-# @staticmethod from_table -> Substitute.from_table()
+if TYPE_CHECKING:
+    from .batch import BatchSubstitution
 
 
-def _substitute_run(
-    run: Run,
-    data: dict | pd.Series,
-    n: int = 0,
-    format: formatType = {},
-    kwargs_fallback: SubConfig | None = None,
-    **kwargs,
-) -> None:
-    """
-    substitute the run's variable by the variable's value
-    """
-    config = SubConfig()
-    config._load_kwargs(**{"format": format, **kwargs})
+class Substitution:
+    def __init__(
+        self,
+        template: Template,
+        data: dict[str, str] | pd.Series,
+        format: formatType = {},
+        n: int = 0,
+        **kwargs,
+    ):
+        self.template = template
+        self.data = data
+        self.format = format
+        self.n = n
+        self.kwargs = kwargs
 
-    if isinstance(data, pd.Series):
-        data = data.to_dict()
+    @staticmethod
+    def _substitute_run(
+        run: Run,
+        data: dict | pd.Series,
+        n: int = 0,
+        format: formatType = {},
+        kwargs_fallback: SubConfig | None = None,
+        **kwargs,
+    ) -> None:
+        """
+        substitute the run's variable by the variable's value
+        """
+        config = SubConfig()
+        config._load_kwargs(**{"format": format, **kwargs})
 
-    def _get(key):
-        return config.get(key, kwargs_fallback)
+        if isinstance(data, pd.Series):
+            data = data.to_dict()
 
-    exclude = _get("exclude")
-    only = _get("only")
+        def _get(key):
+            return config.get(key, kwargs_fallback)
 
-    if len(exclude) > 0 and len(only) > 0:
-        raise Exception("can not use `vars_exclusion` and `vars_only` at the same time")
+        exclude = _get("exclude")
+        only = _get("only")
 
-    text = run.text
-    matches = re.finditer(r"{(([\w\s\d\(\)°\.]+);?(\w*))}", text)
+        if len(exclude) > 0 and len(only) > 0:
+            raise Exception("can not use `vars_exclusion` and `vars_only` at the same time")
 
-    for m in matches:
-        # ph: '{variable;format1}'
-        ph = m.group()
-        # variable keys syntax
-        #
-        # full_key: 'variable;format1'
-        # value_key: 'variable'
-        # specific_key: 'format1'
-        full_key, value_key, specific_key = m.groups()
-        if value_key in exclude:
-            continue
-        if value_key not in data:
-            continue
-        if len(only) > 1 and value_key not in only:
-            continue
+        text = run.text
+        matches = re.finditer(r"{(([\w\s\d\(\)°\.]+);?(\w*))}", text)
 
-        # format
-        #
-        # not using specific_key at the moment. full_key is enough
-        ffunc: Callable
-        if full_key in format:
-            ffunc = format[full_key]
-        elif specific_key in functions:
-            ffunc = functions[specific_key]
-        else:
-            ffunc = lambda x: x
+        for m in matches:
+            # ph: '{variable;format1}'
+            ph = m.group()
+            # variable keys syntax
+            #
+            # full_key: 'variable;format1'
+            # value_key: 'variable'
+            # specific_key: 'format1'
+            full_key, value_key, specific_key = m.groups()
+            if value_key in exclude:
+                continue
+            if value_key not in data:
+                continue
+            if len(only) > 1 and value_key not in only:
+                continue
 
-        # value
-        #
-        # get value for n-th numeric variable
-        if n > 0:
-            value_key = re.sub("\\b1(?!°)\\b", str(n), value_key)
+            # format
+            #
+            # not using specific_key at the moment. full_key is enough
+            ffunc: Callable
+            if full_key in format:
+                ffunc = format[full_key]
+            elif specific_key in functions:
+                ffunc = functions[specific_key]
+            else:
+                ffunc = lambda x: x
 
-        # new value
-        new_val = str(ffunc(data[value_key]))
+            # value
+            #
+            # get value for n-th numeric variable
+            if n > 0:
+                value_key = re.sub("\\b1(?!°)\\b", str(n), value_key)
 
-        text = text.replace(ph, new_val)
+            # new value
+            new_val = str(ffunc(data[value_key]))
 
-    # replacing value
-    run.text = text
+            text = text.replace(ph, new_val)
+
+        # replacing value
+        run.text = text
+
+    def render(self) -> Document:
+        """substitute all variables in the document"""
+
+        temp_id = Template._loaded_templates.index(self.template)
+        norm_temp = Template._normalized_templates[temp_id]
+        new_document = norm_temp.copy()
+
+        for run in iter_runs(new_document):
+            if "{" in run.text:
+                self._substitute_run(
+                    run=run,
+                    data=self.data,
+                    n=self.n,
+                    format=self.format,
+                    kwargs_fallback=self.template.config,
+                    **self.kwargs,
+                )
+
+        return new_document
+
+    @classmethod
+    def from_table(
+        cls,
+        temp: Template | list[Template],
+        table: pd.DataFrame,
+        naming_schema: str | Callable,
+        format: formatType = {},
+        parent_directory=None,
+        pdf: bool = True,
+        zip: bool = False,
+        **kwargs,
+    ) -> "BatchSubstitution":
+        from .batch import BatchSubstitution
+
+        return BatchSubstitution(
+            temp=temp,
+            table=table,
+            naming_schema=naming_schema,
+            format=format,
+            parent_directory=parent_directory,
+            pdf=pdf,
+            zip=zip,
+            substitution_cls=cls,
+            **kwargs,
+        )
 
 
 def Substitute(
@@ -94,29 +152,4 @@ def Substitute(
     n: int = 0,
     **kwargs,
 ) -> Document:
-    """substitute all variables in the document"""
-
-    temp_id = Template._loaded_templates.index(temp)
-    norm_temp = Template._normalized_templates[temp_id]
-    new_document = norm_temp.copy()
-
-    # TODO make a class SubstitutionConfig ?
-    # it should not modify Temp attributes, since this options should be temporary
-    #
-    # method to get value of parameter from 'self' and 'other' class instances
-    # use: substitution input parameter -> self
-    #      template default values as fallback -> other
-    # returns self.x unless self.x is None, when it returns other.x
-    
-    for r in iter_runs(new_document):
-        if "{" in r.text:
-            _substitute_run(
-                run=r,
-                data=data,
-                n=n,
-                format=format,
-                kwargs_fallback=temp.config,
-                **kwargs,
-            )
-
-    return new_document
+    return Substitution(temp, data, format=format, n=n, **kwargs).render()
